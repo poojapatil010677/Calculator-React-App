9f4bfdf57c0353b97aabe0ef920eb27fe11753d2
Pooja Patil <poojapatil010677@gmail.com>
2020-06-13 19:39:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Keypad from './Keypad'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+      currentNum:''
+    };
+    this.updateNum = this.updateNum.bind(this);
+  }
+
+  evaluate() 
+  {
+    try {
+    const val = this.state.currentNum;
+    // eslint-disable-next-line
+    const result = eval(val);
+    console.log(result);
+    this.setState({currentNum:result});
+  }
+  catch(e)
+  {
+    this.setState({currentNum:"ERROR"});
+  }
+  }
+
+  updateNum(num)
+  {   
+      console.log("hello");
+      if(num==="=")
+      {
+          console.log(this.state.currentNum);
+          this.evaluate();
+      }
+      else if(num==="C")
+      {
+        console.log(this.state.currentNum);
+        this.setState({currentNum:''});
+      }
+      else if(num=="CE")
+      {
+        this.setState({currentNum:this.state.currentNum.slice(0,-1)});
+      }
+      else
+      {
+        const val = this.state.currentNum + num;
+        this.setState({currentNum:val});
+      }
+  }
+
+  render()
+  {
+    return (
+        <div className="calci">
+          <div className="calci-head">
+            <form id="calci-form">
+              <input type="text" value={this.state.currentNum}></input>
+            </form>
+          </div>
+          <Keypad updateNum={this.updateNum}></Keypad>
+        </div>
+      );
+  }
 }
 
 export default App;
