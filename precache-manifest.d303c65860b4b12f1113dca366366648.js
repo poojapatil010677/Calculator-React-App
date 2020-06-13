beb0a64335b10e87cee13ca2529d3623a6b002da
Pooja Patil <poojapatil010677@gmail.com>
2020-06-13 19:44:01 +0530

    Updates

---
+++ precache-manifest.d303c65860b4b12f1113dca366366648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2eb0ef208e65287376f6b934dde5342",
+    "url": "/Calculator-React-App/index.html"
+  },
+  {
+    "revision": "c3644330a7799173ffdf",
+    "url": "/Calculator-React-App/static/css/main.4d5e50ba.chunk.css"
+  },
+  {
+    "revision": "3e75ceef08ecd1f2d118",
+    "url": "/Calculator-React-App/static/js/2.86ce47b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-React-App/static/js/2.86ce47b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3644330a7799173ffdf",
+    "url": "/Calculator-React-App/static/js/main.0be53452.chunk.js"
+  },
+  {
+    "revision": "6a0bd9b84eb678faff1b",
+    "url": "/Calculator-React-App/static/js/runtime-main.c6c4f33e.js"
+  }
+]);
